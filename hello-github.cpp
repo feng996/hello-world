5c274e1167f56f7b1d11f00610a2cfb77c501068
feng996 <1462499364@qq.com>
2019-08-04 17:57:14 +0800

    8/4

--- hello-github.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main {
-  cout << "hello github" << endl;
-  cout << "change file" << endl;
-}
--- temp.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-  public static void main(String... strings) {
-    System.out.print("hello world");
-  }
-}
